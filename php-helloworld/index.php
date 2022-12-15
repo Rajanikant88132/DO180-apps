bc9c0666387033b5c8682f91d673439b50d898e4
Student User <student@workstation.lab.example.com>
2022-12-15 10:05:57 -0500

    rajanikant

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,8 +1,3 @@
 <?php
-<<<<<<< HEAD
 print "Hello, World! Rajanikant php version is " . PHP_VERSION . "\n";
-=======
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is coming 2!\n";
->>>>>>> cfe3b055a57a9497b3cbc983b3cdc45d7e8b6c69
 ?>
